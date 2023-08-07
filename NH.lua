6b87d6371f8b7e6f3e5bff49ab1d44b78d1fa205
Sqxare <94962535+Sqxare@users.noreply.github.com>
2023-08-07 19:18:33 -0400

    Update NH.lua

--- NH.lua
+++ NH.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/4a1bb74be04d38809f82943eeff690d2.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/00c1431c0295f44e3fb9e8dea7e7170d.lua"))()
